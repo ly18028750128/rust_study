ee484bedff9817f1fbcba3c770980b1849a664c6
ly18028750128 <87405411+ly18028750128@users.noreply.github.com>
2024-06-24 12:12:51 +0800

    11

--- src/test_lambda.rs
+++ src/test_lambda.rs
@@ -1,7 +1,7 @@
 pub fn exec_lambda(a: u32, b: u32, c: u32) -> u32 {
     let calculate = |a, b, c| a * b + c;
     let mut idx:i64 = 0;
-    while idx < 100_0000_0000 {
+    while idx < 100 {
         // 循环体
         calculate(a, b, c);
         idx += 1;
